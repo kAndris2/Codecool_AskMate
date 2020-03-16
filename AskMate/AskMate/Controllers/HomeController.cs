09ac1f62835fce0484725ad4de7f4ef5849fd20f
burgonyapure <deejay.spory@gmail.com>
2020-03-16 13:33:10 +0100

    indexcshtml

--- AskMate/AskMate/Controllers/HomeController.cs
+++ AskMate/AskMate/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace AskMate.Controllers
 {
     public class HomeController : Controller
     {
+        
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
