a78debcbe0f627a88a5f55ef0de845080cfd0fc4
kAndris2 <k.andris258@gmail.com>
2020-03-19 13:33:11 +0100

    answer imglink added

--- AskMate/AskMate/Models/AnswerModel.cs
+++ AskMate/AskMate/Models/AnswerModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AskMate;
 
 namespace AskMate.Models
 {
@@ -10,6 +11,7 @@ namespace AskMate.Models
         public int Id { get; }
         public int Vote { get; private set; }
         public String Content { get; private set; }
+        public String ImgLink { get; private set; }
         public long Date { get; }
 
         public AnswerModel(int id, string content, long date, int vote)
@@ -21,15 +23,21 @@ namespace AskMate.Models
         }
 
         public DateTime GetDate() { return new DateTime(1970, 1, 1).AddMilliseconds(double.Parse(Date.ToString())); }
-
-        public override string ToString()
-        {
-            return $"{Id}.{Content}.{Date}.{Vote}";
-        }
+        public void AddImage(string link) { ImgLink = link; Refr(); }
 
         public long GetUnique()
         {
             return Convert.ToInt64($"{Id}{Date}");
+        }
+
+        private void Refr()
+        {
+            IDAO_Impl.Instance.Refresh();
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}.{Content}.{Date}.{Vote}";
         }
     }
 }
