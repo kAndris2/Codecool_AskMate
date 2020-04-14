1f2de4e2fb32e5260f099f89894f6d997aa3e5fb
kAndris2 <k.andris258@gmail.com>
2020-04-14 09:33:40 +0200

    UserModel

---
+++ AskMate/AskMate/Models/UserModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskMate.Models
+{
+    public class UserModel
+    {
+        public String Email { get; private set; }
+        public String Name { get; private set; }
+        public String Password { get; private set; }
+        public long Date { get; }
+        public int Reputation { get; private set; }
+
+        public UserModel(string email, string name, string password, long date)
+        {
+            Email = email;
+            Name = name;
+            Password = password;
+            Date = date;
+        }
+
+        public UserModel(string email, string name, string password, long date, int reputation)
+        {
+            Email = email;
+            Name = name;
+            Password = password;
+            Date = date;
+            Reputation = reputation;
+        }
+
+        public void IncreaseReputation(int value) { Reputation += value; }
+        public void DecreaseReputation(int value) { Reputation -= value; }
+    }
+}
